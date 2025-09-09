95600abcf2f8c0492f12f8b38e60a7191c8b0a1e
Eric L <eric@joinformal.com>
2025-09-08 21:15:29 -0700

    Remove <method>Context since it makes it incompatible with net/rpc

--- client/client.go
+++ client/client.go
@@ -1,8 +1,5 @@
 package client
 
-import "context"
-
 type Client interface {
 	Call(method string, args interface{}, reply interface{}) error
-	CallContext(ctx context.Context, method string, args interface{}, reply interface{}) error
 }
